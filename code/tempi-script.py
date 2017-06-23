47eafadd412b40f9c0a6ea2309492e267716b267
Markus Napp <mnapp@arago.de>
2017-06-23 16:55:27 +0200

    Added cpu temp measuring

--- code/tempi-script.py
+++ code/tempi-script.py
@@ -1,12 +1,13 @@
 import time
 import math
+import vcgencmd
 from datetime import datetime
 from envirophat import light, motion, weather, leds
 
 
 out = open('enviro.log', 'w')
 #out.write('time\tlight\trgb\tmotion\theading\ttemp\tpress\n')
-out.write('Time\tTemperature\tTemp (Rounded)\tLight\tPressure\n')
+out.write('Time\tTemp (Raw)\tTemp (Rounded)\tCPU Temp\tLight\tPressure\n')
 
 try:
     while True:
@@ -17,12 +18,14 @@ try:
         leds.off()
         acc = str(motion.accelerometer())[1:-1].replace(' ', '')
         heading = motion.heading()
-        temp = weather.temperature() - 6.1
+        cpu_temp = vcgencmd.measure_temp()
+        temp = weather.temperature() - 5.8
+        raw = weather.temperature()
         tempmed = round(temp, 0)
         #tempmed = 0.5 * math.ceil(2.0 * weather.temperature())
         press = weather.pressure()
         #out.write('%s\t%f\t%s\t%s\t%f\t%f\t%f\n' % (currtime, lux, rgb, acc, heading, temp, press))
-        out.write('%s\t%f\t%i\t%s\t%f\n' % (currtime, temp, tempmed, lux, press))
+        out.write('%s\t%f\t%i\t%f\t%s\t%f\n' % (currtime, raw, tempmed, cpu_temp, lux, press))
         out.flush()
         time.sleep(20)
 
