338d5a201b97d24243834ce609e61e625fb4e540
Markus Napp <mnapp@arago.de>
2017-06-24 22:03:59 +0200

    Updated output for one decimal calibrated temp

--- code/tempi-script.py
+++ code/tempi-script.py
@@ -24,11 +24,11 @@ try:
         cpu_temp = vcgencmd.measure_temp()
         temp = weather.temperature()
         temp_calibrated = temp - ((cpu_temp - temp)/factor)
-        tempmed = round(temp_calibrated, 0)
+        tempmed = '{:.1f}'.format(round(temp_calibrated, 2))
         #tempmed = 0.5 * math.ceil(2.0 * weather.temperature())
         press = weather.pressure()
         #out.write('%s\t%f\t%s\t%s\t%f\t%f\t%f\n' % (currtime, lux, rgb, acc, heading, temp, press))
-        out.write('%s\t%f\t%f\t%i\t%f\t%s\n' % (currtime, temp, temp_calibrated, tempmed, cpu_temp, lux))
+        out.write('%s\t%f\t%f\t%s\t%f\t%s\n' % (currtime, temp, temp_calibrated, tempmed, cpu_temp, lux))
         out.flush()
         message = "It is " + str(currtime) + "\nThe current temperature is " + str(tempmed) + " measuring " + str(lux) + " lux of light"
         payload = {'text':message}
