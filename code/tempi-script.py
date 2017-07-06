7575c095bffc2126587e97a9a75c78c54a470587
Markus Napp <mnapp@arago.de>
2017-07-06 13:36:13 +0200

    Updated script, added pyowm to todo, added owm mapping draft

--- code/tempi-script.py
+++ code/tempi-script.py
@@ -4,10 +4,14 @@ import vcgencmd
 import os
 import json
 import requests
+import schedule
 
 from datetime import datetime
 from envirophat import light, weather, leds
 
+# raspi = :raspberry_pi:
+
+# Declaring global variables
 temp = 0
 tempmed = 0
 temp_calibrated = 0
@@ -17,6 +21,7 @@ outside_location = ""
 outside_condition = ""
 currtime = datetime.now().strftime('%H:%M:%S')
 
+# Slack Webhook stored in filesystem
 slack_webhook = os.environ["SLACK_WEBHOOK"]
 
 # Data for Openweathermap Query
@@ -28,10 +33,23 @@ unit = "metric"
 # Sensor offset factor
 factor = 0.868218182
 
+# Open log file for temp data
 out = open('enviro.log', 'w')
 out.write('Time\tTemp (Sensor)\tTemp (Calibrated)\tTemp (Rounded)\tCPU Temp\n')
 
+def get_condition():
+    """
+    TODO
+
+    Reads current condition and maps emoji and icons to the message which is to be sent to Slack.
+    """
+
 def get_outside():
+    """
+    Polls the weather data from openweathermap API and returns values for global usage.
+
+    Will be refactored to use pyowm instead of requests.
+    """
     global outside_temp
     global outside_condition
     global outside_location
@@ -60,9 +78,7 @@ def send_message():
     out_temp = str(outside_temp)
     med = str(tempmed)
 
-    post = """It is currently %s in %s, %s
-    Outside there is %s at %s C
-    Inside the office we have a temperature of %s C
+    post = """It is currently %s in %s, %s\nOutside there is "%s" at %s C\nInside the office we have a temperature of %s C
     """
 
     message = post % (mytime, zip_code, outside_location, outside_condition, out_temp, med)
@@ -76,11 +92,6 @@ def write_file():
     out.write('%s\t%f\t%f\t%s\t%f\n' % (currtime, temp, temp_calibrated, tempmed, cpu_temp))
 
 def write_json():
-    global outside_temp
-    global outside_condition
-    global outside_location
-    global tempmed
-    global currtime
     date = datetime.now().strftime('%d-%m-%Y')
 
     json_file = open('weather.json', 'w')
@@ -110,12 +121,19 @@ def perform_update():
     out.flush()
     leds.off()
 
+# Schedule messages
+schedule.every().day.at("08:00").do(send_message)
+schedule.every().day.at("12:00").do(send_message)
+
 try:
     while True:
         perform_update()
+
+        schedule.run_pending()
 
         time.sleep(30)
 
 except KeyboardInterrupt:
     leds.off()
     out.close()
+    # json_file.close()
