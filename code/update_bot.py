c54ba73ead46f7f08ecd699fc3310e4216df9e27
Markus Napp <mnapp@arago.de>
2017-07-25 15:41:19 +0200

    Read and write known versions from file.

--- code/update_bot.py
+++ code/update_bot.py
@@ -4,6 +4,30 @@ import schedule
 import telepot
 import time
 import os
+import ast
+from pprint import pprint
+import pdb
+import json
+
+known = ''
+known_versions = {}
+
+def get_current(tp):
+    global known_versions
+    with open('known_versions.json','r') as verfile:
+        known_versions = json.load(verfile)
+        known = str(known_versions[tp][-1])
+
+def append_known(tp, version):
+    global known_versions
+
+    if version not in known_versions[tp]:
+        known_versions[tp].append(unicode(version))
+        with open('known_versions.json','w') as verfile:
+            verlist = json.dumps(known_versions, indent=4, sort_keys=True)
+            verfile.write(verlist)
+    else:
+        print("Version already known.")
 
 def send_bot_msg(message):
     user_id = os.environ['USER_ID']
@@ -11,7 +35,8 @@ def send_bot_msg(message):
     bot.sendMessage(user_id, message)
 
 def find_g870a_update():
-    known = 'G870AUCS2DQD1'
+    get_current('g870a')
+
     headers = {'Accept': 'application/json'}
 
     r = requests.get('https://services.att.com/kmservices/v2/contents/KM1126238?app-id=esupport', headers=headers)
@@ -27,25 +52,23 @@ def find_g870a_update():
     # Current version
     pos = xml.find("Baseband version:",0)
     verpos = xml.find("G870A",pos+17,pos+57)
-    current_version = xml[verpos:verpos+13]
+    version = xml[verpos:verpos+13]
 
     # Previous version
     prev = xml.rfind("Previous versions required:",0)
     prev_ver = xml.find("G870A",prev+27)
     previous_version = xml[prev_ver:prev_ver+13]
 
-    url =  'https://xdmd.sl.attcompute.com/agents/42998/1488/SS-' + previous_version + '-to-' + current_version[7:] + '-UP'
-    message = 'A new update for the G870A is released:\n' + current_version + '\n' + ')\nDownload here: ' + url
+    url =  'https://xdmd.sl.attcompute.com/agents/42998/1488/SS-' + previous_version + '-to-' + version[7:] + '-UP'
+    message = 'A new update for the G870A is released:\n' + version + '\nDownload here: ' + url
 
-    if current_version != known:
+    if version != known:
         send_bot_msg(message)
         print(url)
-        known = current_version
-
-    # print (current_version + ' ' + previous_version + ' ' + version_changes)
+        append_known('g870a', version)
 
 def find_qbt_update():
-    known = 'v3.3.14'
+    get_current('qbt')
 
     html = requests.get('https://www.qbittorrent.org/news.php')
     soup = BeautifulSoup(html.text,'html.parser')
@@ -61,7 +84,7 @@ def find_qbt_update():
     if version != known:
         send_bot_msg(message)
         print(url)
-        known = version
+        append_known('qbt', version)
 
 schedule.every().day.at("12:00").do(find_g870a_update)
 schedule.every().day.at("12:00").do(find_qbt_update)
@@ -76,4 +99,4 @@ try:
         time.sleep(30)
 
 except KeyboardInterrupt:
-    print "Crashed!"
+    print "Terminated!"
